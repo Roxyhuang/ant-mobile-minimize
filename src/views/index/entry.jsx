0933bca89cec972c3f7ab883d514b2b1d6175741
roxy_huang <sixin_huang@outlook.com>
2018-06-01 09:01:44 +0800

    update fastclick

--- src/views/index/entry.jsx
+++ src/views/index/entry.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'assets/css/global.less';
 import { Provider } from 'react-redux';
 import zhCN from 'antd-mobile/lib/locale-provider/';
+import attachFastClick from 'fastclick';
 import { LocaleProvider } from 'antd-mobile';
 import createStore from '../../core/createStore';
 import routers from '../../routes/route';
@@ -12,6 +13,8 @@ if (process.env.NODE_ENV === 'development' && process.env.DEBUG) {
   // open debug mode
   eruda.init();
 }
+
+attachFastClick.attach(document.body);
 
 const store = createStore();
 
